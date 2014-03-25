2966aa448b33238180ae01a08f9da906f62eb433
gsick <gsick@emailvision.com>
2014-03-25 11:33:57 +0100

    add unit/smoke test

--- spec/classes/btsync_spec.rb
+++ spec/classes/btsync_spec.rb
@@ -54,10 +54,62 @@ describe 'btsync' do
 
     it do
       should contain_exec('btsync').with({
-        'ensure'  => 'present',
         'cwd'    => '/opt/btsync',
         'command'    => '/opt/btsync/btsync --config btsync.json',
-        'require' => ['Exec[untar btsync]', 'File[btsync conf file, btsync conf dir]'],
+        'require' => ['Exec[untar btsync]', 'File[btsync conf file]', 'File[btsync conf dir]'],
+      })
+    end
+
+  end
+
+  context "with glibc23 param" do
+    let(:params) { {:glibc23 => false} }
+
+    it do
+      should contain_exec('download btsync').with({
+        'cwd'     => '/tmp',
+        'path'    => '/bin:/usr/bin',
+        'command' => 'wget -O btsync_x64.tar.gz http://download-lb.utorrent.com/endpoint/btsync/os/linux-x64/track/stable',
+        'creates' => '/tmp/btsync_x64.tar.gz',
+        'notify'  => 'Exec[untar btsync]',
+        'require' => 'Package[wget]',
+      })
+    end
+
+    it do
+      should contain_exec('untar btsync').with({
+        'cwd'     => '/tmp',
+        'path'    => '/bin:/usr/bin',
+        'command' => 'tar -zxvf btsync_x64.tar.gz -C /opt/btsync',
+        'creates' => '/opt/btsync/btsync',
+        'require' => 'File[btsync install dir]',
+      })
+    end
+
+  end
+
+  context "with arch param" do
+    let(:params) { {:glibc23 => false} }
+    let(:facts) { {:architecture => 'i386'} }
+
+    it do
+      should contain_exec('download btsync').with({
+        'cwd'     => '/tmp',
+        'path'    => '/bin:/usr/bin',
+        'command' => 'wget -O btsync_i386.tar.gz http://download-lb.utorrent.com/endpoint/btsync/os/linux-i386/track/stable',
+        'creates' => '/tmp/btsync_i386.tar.gz',
+        'notify'  => 'Exec[untar btsync]',
+        'require' => 'Package[wget]',
+      })
+    end
+
+    it do
+      should contain_exec('untar btsync').with({
+        'cwd'     => '/tmp',
+        'path'    => '/bin:/usr/bin',
+        'command' => 'tar -zxvf btsync_i386.tar.gz -C /opt/btsync',
+        'creates' => '/opt/btsync/btsync',
+        'require' => 'File[btsync install dir]',
       })
     end
 
